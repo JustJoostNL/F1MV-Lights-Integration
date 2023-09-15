886d8cdb423e6ac1c316f84d2a38da90792f905c
Joost <git@jstt.me>
2023-09-15 23:01:53 +0200

    fix

--- build/notarize.js
+++ build/notarize.js
@@ -29,9 +29,6 @@ module.exports = async (context) => {
       teamId: process.env.APPLE_TEAM_ID,
       appleId: process.env.APPLE_ID,
       appleIdPassword: process.env.APPLE_ID_PASS,
-      // appleApiKey: process.env.APPLE_API_KEY,
-      // appleApiKeyId: process.env.APPLE_API_KEY_ID,
-      // appleApiIssuer: process.env.APPLE_API_ISSUER,
     });
   } catch (error) {
     console.error(error);
